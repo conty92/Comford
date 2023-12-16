84151d01fabc9b4396d5d21653f3ca2436885449
ahmad <dardaumusaahmad111@gmail.com>
2023-12-16 17:52:40 +0100

    modefief

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,14 @@
+
+import "./header.css";
 const Header = () => {
     return ( 
-        <>
+        <section>
        <div className="logo"><p>comford</p></div> 
        <div className="ancho"></div>
        <div className="login">login</div>
         
         
-        </>
+        </section>
 
 
      );
