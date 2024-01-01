b3ba230603bca5792bc3dbf7e86e1ceb116b35df
ahmad <dardaumusaahmad111@gmail.com>
2024-01-01 17:45:32 +0100

    modified

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,19 @@
-import  "../styles/Header.css"
+// import  "../styles/Header.css"
+import { FaEnvelope } from "react-icons/fa";
+
+import { FiPhone } from "react-icons/fi";
+
+
 const Header = () => {
     return ( 
-       <div className="bg-red-300">
-
-
+       <div className="flex">
+        <div className="flex w-1/3 bg-green-400 ">
+        <p className="flex items-center "> <FiPhone />07061996492</p>
+        <p className="flex items-center "> <FaEnvelope />jabow@hfkjbkkbkn</p>
+        </div>
+        <div>
+          
+        </div>
        </div>
 
 
